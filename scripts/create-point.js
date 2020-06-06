b53cfa3375684043ba4e2e959d0a7a661b515460
IgorBatista <contato@igorbatista.com.br>
2020-06-06 02:21:17 -0300

    UPdate

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -29,7 +29,6 @@ function getCities(event) {
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
   fetch(url)
-
   .then(res => res.json())
   .then(cities => {
 
